bc63c8ad0a855574a5d78ba86546b1ff5e063d0c
Marc-Eid <marc.mn.eid@gmail.com>
2023-01-26 15:25:32 -0500

    removed Unnessary keys.

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ mycursor = conn.cursor()
 config = configparser.ConfigParser()
 config.read('venv/config.ini')
 
-api_key = config['twitter']['api_key']
-api_key_secret = config['twitter']['api_key_secret']
-
-access_token = config['twitter']['access_token']
-access_token_secret = config['twitter']['access_token_secret']
+# api_key = config['twitter']['api_key']
+# api_key_secret = config['twitter']['api_key_secret']
+#
+# access_token = config['twitter']['access_token']
+# access_token_secret = config['twitter']['access_token_secret']
 
 bearer_token =  config['twitter']['bearer_token']
 
