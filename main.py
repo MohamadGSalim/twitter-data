f8839597bf4e12605393c22ce2a57817b6c3d351
Marc-Eid <marc.mn.eid@gmail.com>
2023-01-27 23:04:26 -0500

    parsed data, ready to hit the Db
    saved data as JSON objects too

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import configparser
 import requests
 import mysql.connector
-
+import json
+from time import sleep
 
 
 #connect to database
@@ -30,28 +31,22 @@ mycursor = conn.cursor()
 #read twitter configs
 config = configparser.ConfigParser()
 config.read('venv/config.ini')
-
-# api_key = config['twitter']['api_key']
-# api_key_secret = config['twitter']['api_key_secret']
-#
-# access_token = config['twitter']['access_token']
-# access_token_secret = config['twitter']['access_token_secret']
-
 bearer_token =  config['twitter']['bearer_token']
 
 
 #api-endpoint
 URL = "https://api.twitter.com/2/tweets/search/recent"
 
-query = "jordan peterson -is:retweet"
+#params
+query = "peterson  -is:retweet"
 expansions = ['author_id']
 media_fields = []
 place_fields = []
 poll_fields = []
 sort_order = ""
-tweet_fields = "public_metrics"
-user_fields = ['profile_image_url,location']
-
+tweet_fields = "public_metrics,created_at,lang,source"
+user_fields = ['created_at,location,public_metrics,verified']
+next_token = None
 
 
 
@@ -59,18 +54,87 @@ PARAMS = {'tweet.fields': tweet_fields,
           'user.fields': user_fields,
           'expansions': expansions,
           'query': query,
-          'max_results': 10
+          'max_results': 10,
+          'next_token': next_token
           }
 
 headers = {"Authorization": "Bearer " + bearer_token}
 
 
 # sending get request and saving the response as response object
-r = requests.get(url = URL, params = PARAMS, headers=headers)
+response = requests.get(url = URL, params = PARAMS, headers=headers)
+json_response = response.json()
 
-print(r.url)
+#the Request URL
+print("URL: \n" + response.url + "\n")
+
+#the tweets
+tweets = json_response['data']
+print("tweets list: ")
+print(tweets)
 print()
-print(r.json())
+
+#the users
+users = json_response['includes']['users']
+print("users list: ")
+print(users)
+print()
+
+#meta data
+print("meta data: ")
+print(json_response['meta'])
+print()
+
+
+
+#file for everything
+with open("json_list.json", "a") as f3:
+    f3.write(",\n")
+    json.dump(json_response, f3)
+
+
+
+#parse tweets save in Json file and save in a DB
+for tweet in tweets:
+
+    #save each tweet in Json File
+    with open("json_tweets_list.json", "a") as f1:
+        f1.write(",\n")
+        json.dump(tweet, f1)
+
+    #parse Tweet
+    tweet_id = tweet['id']
+    tweet_text = tweet['text']
+
+
+    #save in Database
+    sql_query = "INSERT IGNORE  INTO tweets (tweet_id, tweet_text) VALUES (%s, %s)"
+    values = (tweet_id, tweet_text)
+
+    mycursor.execute(sql_query, values)
+    conn.commit()
+
+
+
+
+# parse users save in Json file and save in a DB
+for user in users:
+
+    # save each tweet in Json File
+    with open("json_users_list.json", "a") as f2:
+         f2.write(",\n")
+         json.dump(user, f2)
+
+    # # parse Tweet
+    tweet_id = user['id']
+    tweet_text = user['username']
+
+    # # save in Database
+    # sql_query = "INSERT IGNORE  INTO users (tweet_id, tweet_text) VALUES (%s, %s)"
+    # values = (tweet_id, tweet_text)
+    #
+    # mycursor.execute(sql_query, values)
+    # conn.commit()
 
 
 
@@ -78,6 +142,73 @@ print(r.json())
 
 
 
+page = 1
+print(f"page {page}")
+
+#paginate through results
+while json_response['meta']['next_token']:
+    sleep(2)
+    next_token = json_response['meta']['next_token']
+    json_response = requests.get(url = URL, params = PARAMS, headers=headers).json()
+
+    # the tweets
+    tweets = json_response['data']
+
+    #the users
+    users = json_response['includes']['users']
+
+
+    # file for everything
+    with open("json_list.json", "a") as f3:
+        f3.write(",\n")
+        json.dump(json_response, f3)
+
+
+    # parse tweets save in Json file and save in a DB
+    for tweet in tweets:
+
+        # save each tweet in Json File
+        with open("json_tweets_list.json", "a") as f1:
+            f1.write(",\n")
+            json.dump(tweet, f1)
+
+        # parse Tweet
+        tweet_id = tweet['id']
+        tweet_text = tweet['text']
+
+        # save in Database
+        # sql_query = "INSERT IGNORE  INTO tweets (tweet_id, tweet_text) VALUES (%s, %s)"
+        # values = (tweet_id, tweet_text)
+        #
+        # mycursor.execute(sql_query, values)
+        # conn.commit()
+
+
+    # parse users save in Json file and save in a DB
+    for user in users:
+
+        # save each tweet in Json File
+        with open("json_users_list.json", "a") as f2:
+            f2.write(",\n")
+            json.dump(user, f2)
+
+        # # parse Tweet
+        tweet_id = user['id']
+        tweet_text = user['username']
+        #
+        # # save in Database
+        # sql_query = "INSERT IGNORE  INTO users (tweet_id, tweet_text) VALUES (%s, %s)"
+        # values = (tweet_id, tweet_text)
+        #
+        # mycursor.execute(sql_query, values)
+        # conn.commit()
+
+    page = page + 1
+
+    print(f"page {page}")
+
+    if page == 5:
+        exit(0)
 
 
 
