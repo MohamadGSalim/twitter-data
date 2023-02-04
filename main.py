a8de101d1be1040ed0202afa305870512b3c2405
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-04 00:36:17 -0500

    fixed a bug. saving users should be done before saving tweets because tweets have a foreign key to users. so if a user doesnt exists the tweet wont be saved

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from time import sleep
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
-    password = "root",
-    port = '3306',
-    database = 'twitter-data'
+    password="root",
+    port='3306',
+    database='twitter-data'
 )
 
 mycursor = conn.cursor()
@@ -31,14 +31,14 @@ mycursor = conn.cursor()
 #read twitter configs
 config = configparser.ConfigParser()
 config.read('venv/config.ini')
-bearer_token =  config['twitter']['bearer_token']
+bearer_token = config['twitter']['bearer_token']
 
 
 #api-endpoint
 URL = "https://api.twitter.com/2/tweets/search/recent"
 
 #params
-query = "peterson  -is:retweet"
+query = "elon musk"
 expansions = ['author_id']
 media_fields = []
 place_fields = []
@@ -47,7 +47,6 @@ sort_order = ""
 tweet_fields = "public_metrics,created_at,lang,entities,referenced_tweets"
 user_fields = ['created_at,location,public_metrics,verified']
 next_token = None
-
 
 
 PARAMS = {'tweet.fields': tweet_fields,
@@ -62,22 +61,22 @@ headers = {"Authorization": "Bearer " + bearer_token}
 
 
 # sending get request and saving the response as response object
-response = requests.get(url = URL, params = PARAMS, headers=headers)
+response = requests.get(url=URL, params=PARAMS, headers=headers)
 json_response = response.json()
 
 #the Request URL
 print("URL: \n" + response.url + "\n")
 
 #the tweets
-tweets = json_response['data']
+tweets_objects = json_response['data']
 print("tweets list: ")
-print(tweets)
+print(tweets_objects)
 print()
 
 #the users
-users = json_response['includes']['users']
+users_objects = json_response['includes']['users']
 print("users list: ")
-print(users)
+print(users_objects)
 print()
 
 #meta data
@@ -86,13 +85,10 @@ print(json_response['meta'])
 print()
 
 
-
 #file for everything
 with open("json_list.json", "a") as f3:
     f3.write(",\n")
     json.dump(json_response, f3)
-
-
 
 
 def handle_tweets(tweets):
@@ -109,54 +105,72 @@ def handle_tweets(tweets):
 
             #parse Tweet
             tweet_id = tweet['id']
+            tweet_author_id = tweet['author_id']
             tweet_text = tweet['text']
             tweet_lang = tweet['lang']
             tweet_created_at = tweet['created_at']
-            tweet_author_id = tweet['author_id']
 
-            #save in db
+            # save in Database
+            sql_query = "INSERT IGNORE  INTO tweets (tweet_id, author_id,text,lang,created_at) VALUES (%s, %s, %s, %s, %s)"
+            values = (tweet_id, tweet_author_id, tweet_text, tweet_lang, tweet_created_at)
+            mycursor.execute(sql_query, values)
 
 
-            #public metrics
+
+            #tweet public metrics
+            tweet_id = tweet_id
             tweet_retweet_count = tweet['public_metrics']['retweet_count']
             tweet_reply_count = tweet['public_metrics']['reply_count']
             tweet_like_count = tweet['public_metrics']['like_count']
             tweet_quote_count = tweet['public_metrics']['quote_count']
             tweet_impression_count = tweet['public_metrics']['impression_count']
 
-            #save in db
+            # save in Database
+            sql_query = "INSERT IGNORE  INTO tweet_public_metric (tweet_id, retweet_count, reply_count, like_count, quote_count, impression_count) VALUES (%s, %s, %s, %s, %s, %s)"
+            values = (tweet_id, tweet_retweet_count, tweet_reply_count, tweet_like_count, tweet_quote_count, tweet_impression_count)
+            mycursor.execute(sql_query, values)
 
-
-            # references
+            # referenced tweets
             if 'referenced_tweets' in tweet:
                 for tweet_reference in tweet['referenced_tweets']:
                     tweet_reference_type = tweet_reference['type']
                     tweet_reference_id = tweet_reference['id']
 
-                    #save in db
+                    # save in Database
+                    sql_query = "INSERT IGNORE  INTO referenced_tweet (ref_tweet_id, type) VALUES (%s, %s)"
+                    values = (tweet_reference_id, tweet_reference_type)
+                    mycursor.execute(sql_query, values)
+
+                    # save in Database
+                    sql_query = "INSERT IGNORE  INTO tweet_references (tweet_id, ref_tweet_id) VALUES (%s, %s)"
+                    values = (tweet_id, tweet_reference_id)
+                    mycursor.execute(sql_query, values)
 
             # hashtags
-            if 'entities in tweet':
-                if 'hashtags' in tweet['entities']:
-                    for tweet_tags in tweet['entities']['hashtags']:
-                        tweet_hashtag = tweet_tags['tag']
+            if 'entities' in tweet and 'hashtags' in tweet['entities']:
+                for tweet_tags in tweet['entities']['hashtags']:
+                    tweet_hashtag = tweet_tags['tag']
 
-                        #save in db
+                    # save in Database
+                    sql_query = "INSERT IGNORE  INTO hashtags (tag) VALUES (%s)"
+                    values = (tweet_hashtag,)
+                    mycursor.execute(sql_query, values)
 
-            #save in Database
-            sql_query = "INSERT IGNORE  INTO tweets (tweet_id, tweet_text) VALUES (%s, %s)"
-            values = (tweet_id, tweet_text)
+                    # save in Database
+                    sql_query = "INSERT IGNORE  INTO tweet_tags (tweet_id, tag) VALUES (%s,%s)"
+                    values = (tweet_id, tweet_hashtag)
+                    mycursor.execute(sql_query, values)
 
-            mycursor.execute(sql_query, values)
             conn.commit()
+
     finally:
         f1.close()
+
 
 def handle_users(users):
 
     try:
         f1 = open("json_users_list.json", "a")
-
 
         # parse users save in Json file and save in a DB
         for user in users:
@@ -171,9 +185,15 @@ def handle_users(users):
             user_name = user['name']
             user_verified = user['verified']
             user_created_at = user['created_at']
+            user_location = None
             if 'location' in user:
                 user_location = user['location']
 
+            # save in Database
+            sql_query = "INSERT IGNORE  INTO users (user_id, username, name, verified, location, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
+            values = (user_id, user_username, user_name, user_verified, user_location, user_created_at)
+
+            mycursor.execute(sql_query, values)
 
             #user public metrics
             user_followers_count = user['public_metrics']['followers_count']
@@ -181,42 +201,42 @@ def handle_users(users):
             user_tweet_count = user['public_metrics']['tweet_count']
             user_listed_count = user['public_metrics']['listed_count']
 
+            # save in Database
+            sql_query = "INSERT IGNORE  INTO user_public_metric (user_id, followers_count, following_count, tweet_count, listed_count) VALUES (%s, %s, %s, %s, %s)"
+            values = (user_id, user_followers_count, user_following_count, user_tweet_count, user_listed_count)
 
+            mycursor.execute(sql_query, values)
 
-
-            # # save in Database
-            # sql_query = "INSERT IGNORE  INTO users (tweet_id, tweet_text) VALUES (%s, %s)"
-            # values = (tweet_id, tweet_text)
-            #
-            # mycursor.execute(sql_query, values)
-            # conn.commit()
+            conn.commit()
 
     finally:
         f1.close()
 
 
 
-
-handle_tweets(tweets)
-handle_users(users)
-
+handle_users(users_objects)
+handle_tweets(tweets_objects)
 
 
 
 page = 1
 print(f"page {page}")
 
-#paginate through results
+# #paginate through results
 while json_response['meta']['next_token']:
     sleep(2)
-    next_token = json_response['meta']['next_token']
-    json_response = requests.get(url = URL, params = PARAMS, headers=headers).json()
+    PARAMS['next_token'] = json_response['meta']['next_token']
+
+    response = requests.get(url = URL, params = PARAMS, headers=headers)
+    json_response = response.json()
+
+    print("URL: \n" + response.url + "\n")
 
     # the tweets
-    tweets = json_response['data']
+    tweets_objects = json_response['data']
 
     #the users
-    users = json_response['includes']['users']
+    users_objects = json_response['includes']['users']
 
 
     # file for everything
@@ -224,12 +244,13 @@ while json_response['meta']['next_token']:
         f3.write(",\n")
         json.dump(json_response, f3)
 
+    # parse users save in Json file and save in a DB
+    handle_users(users_objects)
 
     # parse tweets save in Json file and save in a DB
-    handle_tweets(tweets)
+    handle_tweets(tweets_objects)
 
-    # parse users save in Json file and save in a DB
-    handle_users(users)
+
 
 
 
@@ -238,10 +259,6 @@ while json_response['meta']['next_token']:
 
     if page == 5:
         exit(0)
-
-
-
-
 
 
 
